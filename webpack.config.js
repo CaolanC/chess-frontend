404a48e26734b95fdcdeae7b12bdb25dca2d4dae
Caolan Cochrane <caolancochrane@gmail.com>
2024-09-16 14:51:39 +0100

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',  // Entry point for your code
+  output: {
+    filename: 'bundle.js',  // The bundled file to be created
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'development',  // Use 'production' for production builds
+};
