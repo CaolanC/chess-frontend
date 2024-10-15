4d2cbb2de3b7b6ea59112a2f5dee474ee8c0693c
Caolan Cochrane <caolancochrane@gmail.com>
2024-10-15 22:44:11 +0100

    pathing change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 "use strict";
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const ChessFrontend_1 = require("./ChessFrontend");
 const board_size = 26;
@@ -11,18 +10,3 @@ if (!parent_container) {
 const board = new ChessFrontend_1.Board(board_size, parent_container, [0xFFCAD4, 0xC5D6D8]);
 board.initApp();
 board.draw();
-(_a = document.getElementById('create-session-form')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
-    event.preventDefault();
-    console.log('o no');
-    fetch('/create-game', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    })
-        .then(response => response.json())
-        .then(data => {
-        window.location.href = data.gameUrl;
-    })
-        .catch(error => {
-        console.error('Error creating game:', error);
-    });
-});
